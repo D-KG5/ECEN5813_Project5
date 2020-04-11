6896489703cc4ff0ff28d4d5be4fd49414dffcf3
sagar <sagar@10.0.0.20>
2020-04-11 16:51:12 -0600

    "UART_interrupt echo and application"

--- ECEN5813_Project5_DK_SE/source/uart_interrupt.h
+++ ECEN5813_Project5_DK_SE/source/uart_interrupt.h
@@ -10,4 +10,26 @@
 
 #include <stdint.h>
 
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL25Z4.h"
+
+#define UART_OVERSAMPLE_RATE 	(16)
+#define BUS_CLOCK 						(24e6)
+#define SYS_CLOCK							(48e6)
+
+
+void Init_UART0_(uint32_t baud_rate);
+
+void UART0_IRQHandler(void);
+
+uint32_t Rx_Chars_Available(void);
+uint8_t	Get_Rx_Char(void);
+void Send_String(uint8_t * str);
+void Receive_String(void);
+uint8_t echo_function();
+uint8_t application_mode();
+
 #endif /* UART_INTERRUPT_H_ */
